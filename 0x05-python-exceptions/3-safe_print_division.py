67e94caf0cea3835a9572131a6344ac8624e94b2
Aissamab <aissamabakkar19@@gmail.com>
2023-11-20 12:25:06 -0800

    this is the second commit

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     resultat = 0
+
     try:
         resultat = a / b
-    except:ZeroDivisionError
+    except ZeroDivisionError:
         resultat = None
     finally:
         print('Inside result: {}'.format(resultat))
